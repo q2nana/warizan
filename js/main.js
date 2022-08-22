15126cdc17b83e4a8c6961bd9a815c79f10ece2d
q2nana <111228096+q2nana@users.noreply.github.com>
2022-08-23 07:25:40 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,23 +10,45 @@
   const tenbtn = document.getElementById('tenbtn');
   const twentybtn = document.getElementById('twentybtn');
   const thirtybtn = document.getElementById('thirtybtn');
-  const yamerubtn = document.getElementById('yamerubtn');
+  const btnbox = document.getElementById('btnbox');
   const countp = document.getElementById('countp');
-  // const play = document.getElementById('play');
+  const count = document.getElementById('count');
+  const yamerubtn = document.getElementById('yamerubtn');
+  const yamerubtn2 = document.getElementById('yamerubtn2');
+  const topbtn = document.getElementById('topbtn');
   const symbol = ['+','-','×','÷'];
   const moudame = document.getElementById('moudame');
-  const topbtn = document.getElementById('topbtn');
+  const moudamebox = document.getElementById('moudamebox');
+
+  yamerubtn2.classList.add('hidden');
+
 
   const num = [];
   let numnum = 10;
-
   const bububu = [];
+
+  let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+  // tenbtn.classList.add('push');
+  // tenbtn.addEventListener('click',()=>{
+  //   console.log('abj');
+  // });
 
   tenbtn.classList.add('push');
 
   ////////////////////////////////////////////////////////////
   //タイマー
   const timetimer = document.getElementById('timetimer');
+  const timetimer2 = document.getElementById('timetimer2');
+  // const startTimer = document.getElementById('startTimer');
+  // const stopTimer = document.getElementById('stopTimer');
+  // const recetTimer = document.getElementById('recetTimer');
   const reslutminutes = document.getElementById('resultminutes');
 
   let startTime;
@@ -53,14 +75,13 @@
   function recet(){
     timetimer.textContent = '00:00.000'
   }
-
   
-  randombtn.addEventListener('click',timerStart);
 
   function timerStart(){
     startTime = Date.now();
     countUp();
   }
+
 
    /////////////////////////////////////////////////
   //何問ゲームするか
@@ -70,7 +91,7 @@
     tenbtn.classList.add('push');
     twentybtn.classList.remove('push');
     thirtybtn.classList.remove('push');
-  })
+  });
 
   twentybtn.addEventListener('click',()=>{
     numnum = 20;
@@ -78,7 +99,7 @@
     twentybtn.classList.add('push');
     tenbtn.classList.remove('push');
     thirtybtn.classList.remove('push');
-  })
+  });
 
   thirtybtn.addEventListener('click',()=>{
     numnum = 30;
@@ -86,133 +107,181 @@
     thirtybtn.classList.add('push');
     tenbtn.classList.remove('push');
     twentybtn.classList.remove('push');
-  })
-
+  });
+  
 
   /////////////////////////////////////////////////
   //問題のセット
-
-
-  // 割るボタン押したとき用
-  function divideSet(){
-    let xright = (Math.floor(Math.random() * 20 + 1 ));
-    let right = (Math.floor(Math.random() * 10 + 1 ));
-    let left = (right * xright); 
-    leftbox.textContent = left;
-    symbolbox.textContent = symbol[3];
-    rightbox.textContent = right;
-  }
 
   
   function questionRandom(){
     answerinput.focus();
     countp.textContent = `あと${numnum - num.length}問！`;
+    let left = (Math.floor(Math.random() * 51 + 1));
+    let right = (Math.floor(Math.random() * 10 + 1));
+    leftbox.textContent = left * right;
     symbolbox.textContent = '÷';
     equalbox.textContent = '=';
-      divideSet();
+    rightbox.textContent = right;
   }
+  
+
+  function display2() {
+    topbtn.classList.add('hidden');
+    btnbox.classList.add('hidden');
+    answerbox.classList.add('display');
+    count.classList.add('display');
+    moudamebox.classList.add('display');
+    numdate.classList.add('display');
+    yamerubtn.classList.add('display');
+  }
+
 
   function btnRemove(){
    if(equalbox.textContent == '=') {
-    randombtn.classList.add('hidden');
-    tenbtn.classList.add('hidden');
-    twentybtn.classList.add('hidden');
-    thirtybtn.classList.add('hidden');
     yamerubtn.classList.add('display');
     yamerubtn.textContent = 'やめる';
     topbtn.classList.add('hidden');
     }
   }
 
+
+
   randombtn.addEventListener('click',questionRandom);
   randombtn.addEventListener('click',btnRemove);
+  randombtn.addEventListener('click',display2);
+  randombtn.addEventListener('click',timerStart);
 
 
-  //たすひくかけるわるそれぞれの計算
+    ////////////////////////////////////////
+  //やめるボタン
 
-    function judge(){
-            let answer = Number(leftbox.textContent) / Number(rightbox.textContent)
-            judge2(answer);
-            judge3(answer);
-        } 
+  function stop(){
+    leftbox.textContent = '';
+    symbolbox.textContent = '';
+    rightbox.textContent = '';
+    equalbox.textContent = '';
+  }
 
-        //正誤判定
+  function reload(){
+    num.length = 0;
+    countStop();
+    recet();
+    nothidden();
+    stop();
+  }
+
+  function display2remove(){
+    topbtn.classList.remove('hidden');
+    btnbox.classList.remove('hidden');
+    answerbox.classList.remove('display');
+    count.classList.remove('display');
+    moudamebox.classList.remove('display');
+    numdate.classList.remove('display');
+    yamerubtn.classList.remove('display');
+    countp.textContent = '';
+  }
+
+  yamerubtn.addEventListener('click',reload);
+  yamerubtn.addEventListener('click',display2remove);
+
+
+
+    //正誤判定
+
+    function seikai() {
+      // console.log('正解！');
+      answerbox.textContent = '正解！';
+      let timer2 = window.setTimeout(doukana,1000);
+      let timer = window.setTimeout(questionRandom,1000);
+      window.setTimeout(remove,1000);
+      answerinput.focus();
+      pinpon();
+      num.push('正解');
+      bububu.length = 0;
+      // console.log(num);
+      moudame.classList.remove('display');
+      countp.textContent = `あと${numnum - num.length}問！`;
+    // if(num.length == numnum){
+    //   window.clearTimeout(timer);
+    //   window.clearTimeout(timer2);
+    //   console.log('2回だよ');
+    //   window.setTimeout(stop,1000);
+    //   window.setTimeout(remove2,1000);
+    // }
+    }
+  
+    function fuseikai() {
+      console.log('ブッブー');
+      answerbox.textContent = 'ブッブー';
+      window.setTimeout(doukana,1000);
+      answerinput.focus();
+      remove();
+      bubu();
+      bububu.push('不正解');
+    }
+
+    function judge2(answer){
+      if(answerinput.value == answer) {
+       seikai();
+    }else {
+       fuseikai();
+      }     
+    }
+
+    function moudamee(answer) {
+      answerbox2.textContent = answer;
+      window.setTimeout(questionRandom,1000);
+      window.setTimeout(remove,1000);
+      answerinput.focus();
+      moudame.classList.remove('display');
+    }
+
+    function judge3(answer){
+      // console.log(bububu);
+      if(bububu.length == 3) {
+        moudame.classList.add('display');
+        bububu.length = 0;
+      }
+
+      moudame.addEventListener('click',() => {
+        moudamee(answer);
+        window.setTimeout(remove2,1000);
+      });
+    }
+
+    function doukana() {
+      answerbox.textContent = '';
+    }
+    
+    function remove(){
+      answerinput.value = '';
+    }
+
+    function remove2(){
+      answerbox.textContent = '';
+    }
+
    
-      
-        function judge2(answer){
-            if(answerinput.value == answer) {
-              console.log('正解！');
-              answerbox.textContent = '正解！';
-              let timer2 = window.setTimeout(doukana,1000);
-              let timer = window.setTimeout(questionRandom,1000);
-              window.setTimeout(remove,1000);
-              answerinput.focus();
-              pinpon();
-              num.push('正解');
-              bububu.length = 0;
-              console.log(num);
-              countp.textContent = `あと${numnum - num.length}問！`;
-              if(num.length == numnum){
-                window.clearTimeout(timer);
-                window.clearTimeout(timer2);
-                console.log('2回だよ');
-                window.setTimeout(stop,1000);
-                window.setTimeout(remove2,1000);
-              }
-            }else {
-              console.log('ブッブー');
-              answerbox.textContent = 'ブッブー';
-              window.setTimeout(doukana,1000);
-              answerinput.focus();
-              remove();
-              bubu();
-              bububu.push('不正解');
-            }     
-        }
+      //たすひくかけるわるそれぞれの計算
 
-       function judge3(answer){
-          console.log(bububu);
-          if(bububu.length == 3) {
-            moudame.classList.add('display');
-          }
-          moudame.addEventListener('click',() => {
-            answerbox.textContent = answer;
-            window.setTimeout(questionRandom,1000);
-            window.setTimeout(remove,1000);
-            window.setTimeout(remove2,1000);
-            answerinput.focus();
-            moudame.classList.remove('display');
-          });
-        }
+      function judge(){
+        let answer = Number(leftbox.textContent) / Number(rightbox.textContent);
+        judge2(answer);
+        judge3(answer);
+      }
+  
 
-        function doukana() {
-          answerbox.textContent = '';
-        }
-        
-        function remove(){
-          answerinput.value = '';
-        }
-
-        function remove2(){
-          answerbox.textContent = '';
-        }
-
-        function stop(){
-          leftbox.textContent = '';
-          symbolbox.textContent = '';
-          rightbox.textContent = '';
-          equalbox.textContent = '';
-        }
-        
-        // answerbtn.addEventListener('click',judge);
-        
-        function enter(event){
-          if(event.key === 'Enter'){
-            judge();
-          }
-        }
-        document.body.addEventListener('keydown',enter);
-        
+    
+    // answerbtn.addEventListener('click',judge);
+    
+    function enter(event){
+      if(event.key === 'Enter'){
+        judge();
+      }
+    }
+    document.body.addEventListener('keydown',enter);
+    
   
 /////////////////////////////////////////////////////////
     //半角数字のみの入力
@@ -234,13 +303,27 @@
 
   const music = new Audio('./クイズ正解1.mp3');
   const music2 = new Audio('./クイズ不正解1.mp3');
+  const soundicon = document.getElementById('soundicon');
+  const soundicon2 = document.getElementById('soundicon2');
+
+  soundicon2.addEventListener('click',()=>{
+    soundicon2.classList.add('hidden');
+  });
+
+  soundicon.addEventListener('click',()=>{
+    soundicon2.classList.remove('hidden');
+  })
  
   function pinpon(){
-    music.play();
+    if(soundicon2.classList.contains('hidden')){
+      music.play();
+    }
   }
 
   function bubu(){
-    music2.play();
+    if(soundicon2.classList.contains('hidden')){
+      music2.play();
+    }
   }
 
 
@@ -255,6 +338,7 @@
       countStop();
       countp.textContent = 'おわり！';
       timetimer.textContent = '';
+     
       window.setTimeout(owari2,1010);
     }
   }
@@ -262,7 +346,7 @@
   function owari2(){
     countp.textContent = '';
     timetimer.textContent = '';
-    yamerubtn.classList.remove('display');
+    stop();
   }
 
   function enter2(event){
@@ -273,15 +357,7 @@
   document.body.addEventListener('keydown',enter2);
 
 
-    ////////////////////////////////////////
-  //やめるボタン
-  yamerubtn.addEventListener('click',reload);
-  yamerubtn.addEventListener('click',()=>{
-    yamerubtn.classList.remove('display');
-    countp.textContent = '';
-    stop();
-    topbtn.classList.remove('hidden');
-  })
+
 
 
   ////////////////////////////////////////
@@ -300,16 +376,10 @@
     btncontain.classList.add('display');
   }
 
-  function reload(){
-    // console.log(num);
-    num.length = 0;
-    countStop();
-    recet();
-    notbtnhidden();
-    nothidden();
-    yamerubtn.classList.remove('display');
-    topbtn.classList.remove('hidden');
-  }
+  
+  btnnext.addEventListener('click',reload);
+  btnnext.addEventListener('click',display2remove);
+  
 
   function nothidden(){
     mask.classList.remove('display');
@@ -318,22 +388,162 @@
     // document.body.removeEventListener('keydown',enter);
   }
 
-  function notbtnhidden(){
-    randombtn.classList.remove('hidden');
-    tenbtn.classList.remove('hidden');
-    twentybtn.classList.remove('hidden');
-    thirtybtn.classList.remove('hidden');
-  }
-
-  btnnext.addEventListener('click',reload);
   btnstop.addEventListener('click',nothidden);
   btnstop.addEventListener('click',()=>{
-    topbtn.classList.remove('hidden');
     yamerubtn.classList.remove('display');
-  });
+    topbtn.classList.remove('hidden');
+  })
+
+///////////////////////////////////////////////////////////
+    // レスポンシブ
+    const numdate = document.getElementById('numdate');
+    const answerbox2 = document.getElementById('answerbox2');
+    const maru = document.getElementById('maru');
+    const batu = document.getElementById('batu');
+
+    function btnRemove2() {
+      if(equalbox.textContent == '=') {
+        timetimer2.classList.remove('hidden');
+        topbtn.classList.add('hidden');
+        yamerubtn.classList.add('hidden');
+        yamerubtn2.classList.add('display');
+        }
+    }
 
 
+    if (window.matchMedia('(max-width: 770px)').matches) {
+      //スマホ処理
+      answerinput.remove();
+      timetimer2.classList.add('hidden');
+      randombtn.addEventListener('click',btnRemove2);
+      randombtn.addEventListener('click',createtable,{once:true});
 
+      yamerubtn2.classList.remove('hidden');
+      yamerubtn2.addEventListener('click',display2remove);
+      yamerubtn2.addEventListener('click',reload);
+      yamerubtn2.addEventListener('click',()=>{
+        timetimer2.classList.add('hidden');
+        topbtn.classList.remove('hidden');
+        countp.textContent = '';
+        answerbox2.textContent = '';
+        yamerubtn2.classList.remove('display');
+      });
+
+
+      btnnext.addEventListener('click',()=> {
+        timetimer2.classList.add('hidden');
+      });
+
+      btnstop.addEventListener('click',()=>{
+        stop();
+        numdate.classList.remove('display');
+       });
+
+      
+
+      
+      
+      function responsiveJudge2(answer){
+        if(answerbox2.textContent == answer) {
+          seikai();
+          answerbox.textContent = '';
+          maru.classList.add('display');
+          window.setTimeout(() => {
+            answerbox2.textContent = '';
+            maru.classList.remove('display');
+          },1010);
+        } else {
+          fuseikai();
+          answerbox.textContent = '';
+          batu.classList.add('display');
+          window.setTimeout(() => {
+            answerbox2.textContent = '';
+            batu.classList.remove('display');
+          },1010);
+        }     
+      }
+
+      function responsiveJudge3(answer){
+        // console.log(bububu);
+        if(bububu.length == 3) {
+          moudame.classList.add('display');
+          bububu.length = 0;
+        }
+  
+        moudame.addEventListener('click',() => {
+          moudamee(answer);
+          window.setTimeout(remove3,1000);
+        });
+      }
+      
+      function responsivejudge(){
+        let answer = Number(leftbox.textContent) / Number(rightbox.textContent);
+        responsiveJudge2(answer);
+        responsiveJudge3(answer);
+      }
+
+      function remove3() {
+        answerbox2.textContent = '';
+      }
+      
+      
+      
+      function createtable(){
+        
+        const table = [];
+        const numtable = [1,2,3,4,5,6,7,8,9,'C',0,'Enter'];
+        // const threetable= numtable.length / 3;
+        for(let n = 0; n < 4; n++) {
+          const tableadd = numtable.splice(0,3);
+          table.push(tableadd);
+          console.log(tableadd);
+        }
+        // console.log(table);
+        
+        while(numdate.firstChild){
+          numdate.removeChild(numdate.firstChild);
+        }
+            table.forEach((table) => {
+              const tr2 = document.createElement('tr');
+              // console.log('tr2')
+              table.forEach((table) => {
+                const td2 = document.createElement('td');
+                // console.log(td2);
+                td2.textContent = table;
+                tr2.appendChild(td2)
+              });
+              numdate.appendChild(tr2);
+            })
+              
+              for(let i = 0; i <= 3; i++) {
+                for(let n = 0; n <=2; n++){
+                  let tablenum = numdate.rows[i].cells[n];
+                  // console.log(tablenum);
+                }
+              }
+              
+              
+              function click(e){
+                // console.log(e.target);
+                if(e.target.textContent == 'C') {
+                  answerbox2.textContent = '';
+                } else if(e.target.textContent =='Enter'){
+                  responsivejudge();
+                  owari();
+                } else {
+                  // console.log(e.target.textContent);
+                  answerbox2.textContent = answerbox2.textContent + e.target.textContent
+                }
+              }
+              
+                numdate.addEventListener('click',click);
+              
+            
+
+               
+            }
+          }
+            
 
 
 
